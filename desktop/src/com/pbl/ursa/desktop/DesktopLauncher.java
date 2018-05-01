6c4a685f1a4ce3ca3e044e57e3e2914184ca139f
Adam Dąbrowski <contact@adamdabrowski.com>
2018-05-02 00:21:34 +0200

    Initial setup

---
+++ desktop/src/com/pbl/ursa/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.pbl.ursa.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.pbl.ursa.UrsaGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new UrsaGame(), config);
+	}
+}
