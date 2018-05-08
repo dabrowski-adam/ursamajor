99cb47de99a94718c3b3d50c17f8686150a75451
Adam Dąbrowski <contact@adamdabrowski.com>
2018-05-08 18:49:50 +0200

    Change window size and disable compass, accelerometer

--- desktop/src/com/pbl/ursa/desktop/DesktopLauncher.java
+++ desktop/src/com/pbl/ursa/desktop/DesktopLauncher.java
@@ -7,6 +7,9 @@ import com.pbl.ursa.UrsaGame;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.title = "Ursa Major";
+		config.width = 320;
+		config.height = 480;
 		new LwjglApplication(new UrsaGame(), config);
 	}
 }
