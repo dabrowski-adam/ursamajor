faef96d609e9c4f077e84706671e4ae95e40392c
Adam Dąbrowski <contact@adamdabrowski.com>
2018-05-13 17:56:29 +0200

    Sample vis-editor setup.

--- core/src/com/pbl/ursa/ScienceGameScreen.java
+++ core/src/com/pbl/ursa/ScienceGameScreen.java
@@ -4,20 +4,27 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.kotcrab.vis.runtime.scene.Scene;
+import com.kotcrab.vis.runtime.scene.VisAssetManager;
 
 public class ScienceGameScreen implements Screen {
     final UrsaGame game;
-    final SpriteBatch spriteBatch;
+    //final SpriteBatch spriteBatch;
+    final VisAssetManager manager;
+    Scene scene;
 
     public ScienceGameScreen(final UrsaGame game) {
         this.game = game;
-        this.spriteBatch = game.spriteBatch;
+        //this.spriteBatch = game.spriteBatch;
+        this.manager = game.manager;
+        scene = manager.loadSceneNow("scene/science.scene");
     }
 
     @Override
     public void render(float delta) {
         Gdx.gl.glClearColor(1, 0.5f, 0.75f, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        scene.render();
 
         if (Gdx.input.isTouched()) {
             game.setScreen(game.mainMenuScreen);
@@ -27,7 +34,7 @@ public class ScienceGameScreen implements Screen {
 
     @Override
     public void resize(int width, int height) {
-
+        scene.resize(width, height);
     }
 
     @Override
--- core/src/com/pbl/ursa/UrsaGame.java
+++ core/src/com/pbl/ursa/UrsaGame.java
@@ -3,15 +3,18 @@ package com.pbl.ursa;
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.kotcrab.vis.runtime.scene.VisAssetManager;
 
 public class UrsaGame extends Game {
     public SpriteBatch spriteBatch;
+    public VisAssetManager manager;
     public BitmapFont font;
     public MainMenuScreen mainMenuScreen;
 
     @Override
     public void create() {
         spriteBatch = new SpriteBatch();
+        manager = new VisAssetManager(spriteBatch);
         font = new BitmapFont(true);
         font.setColor(0.1f, 0.1f, 0.1f, 1);
         mainMenuScreen = new MainMenuScreen(this);
@@ -26,6 +29,7 @@ public class UrsaGame extends Game {
     @Override
     public void dispose() {
         spriteBatch.dispose();
+        manager.dispose();
         font.dispose();
         mainMenuScreen.dispose();
     }
